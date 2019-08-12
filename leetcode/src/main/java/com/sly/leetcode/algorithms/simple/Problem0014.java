c3c2f9c1007e4a2ced9cf28eb1132f4784aef7e6
sly <1311220942@qq.com>
2019-08-12 21:56:11 +0800

    提交

---
+++ leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0014.java
@@ -0,0 +1,29 @@
+package com.sly.leetcode.algorithms.simple;
+
+/**
+ *<p> 编写一个函数来查找字符串数组中的最长公共前缀。</p>
+ *<p>如果不存在公共前缀，返回空字符串 ""。</p>
+ *<p>示例 1:</p>
+ *<p>输入: ["flower","flow","flight"]</p>
+ *<p>输出: "fl"</p>
+ *<p>示例 2:</p>
+ *<p>输入: ["dog","racecar","car"]</p>
+ *<p>输出: ""</p>
+ *<p>解释: 输入不存在公共前缀。</p>
+ *<p>说明:</p>
+ *<p>所有输入只包含小写字母 a-z 。</p>
+ *<p>来源：力扣（LeetCode）</p>
+ *<p>链接：https://leetcode-cn.com/problems/longest-common-prefix</p>
+ *<p>著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。</p>
+ * @author sly
+ * @time 2019年8月12日
+ */
+public class Problem0014 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String longestCommonPrefix(String[] strs) {
+        return null;
+    }
+}
