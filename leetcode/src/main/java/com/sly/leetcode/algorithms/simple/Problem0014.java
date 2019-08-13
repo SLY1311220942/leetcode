fdba4ff0a620aaa4629875a5fda4b1289ef24b89
盛领宜 <sheng.ly_wh@www.sand.com>
2019-08-13 16:43:37 +0800

    提交

--- leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0014.java
+++ leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0014.java
@@ -1,29 +1,74 @@
 package com.sly.leetcode.algorithms.simple;
 
 /**
- *<p> 编写一个函数来查找字符串数组中的最长公共前缀。</p>
- *<p>如果不存在公共前缀，返回空字符串 ""。</p>
- *<p>示例 1:</p>
- *<p>输入: ["flower","flow","flight"]</p>
- *<p>输出: "fl"</p>
- *<p>示例 2:</p>
- *<p>输入: ["dog","racecar","car"]</p>
- *<p>输出: ""</p>
- *<p>解释: 输入不存在公共前缀。</p>
- *<p>说明:</p>
- *<p>所有输入只包含小写字母 a-z 。</p>
- *<p>来源：力扣（LeetCode）</p>
- *<p>链接：https://leetcode-cn.com/problems/longest-common-prefix</p>
- *<p>著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。</p>
+ * <p>
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * </p>
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * </p>
+ * <p>
+ * 示例 1:
+ * </p>
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * </p>
+ * <p>
+ * 输出: "fl"
+ * </p>
+ * <p>
+ * 示例 2:
+ * </p>
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * </p>
+ * <p>
+ * 输出: ""
+ * </p>
+ * <p>
+ * 解释: 输入不存在公共前缀。
+ * </p>
+ * <p>
+ * 说明:
+ * </p>
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ * </p>
+ * 
  * @author sly
  * @time 2019年8月12日
  */
 public class Problem0014 {
 	public static void main(String[] args) {
-		
+		//String[] strs = {"dog","racecar","car"};
+		//String[] strs = {"flower","flow","flight"};
+		String[] strs = {"flowable","flow","flowing"};
+		System.out.println(longestCommonPrefix(strs));	
 	}
-	
+
 	public static String longestCommonPrefix(String[] strs) {
-        return null;
-    }
+		if(strs == null || strs.length < 1) {
+			return "";
+		}
+		String minStr = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+			if (minStr.length() > strs[i].length()) {
+				minStr = strs[i];
+			}
+		}
+		int index = 0;
+		for (int i = 0; i < minStr.length(); i++) {
+			for (int j = 0; j < strs.length; j++) {
+				if (minStr.charAt(i) != strs[j].charAt(i)) {
+					index = i;
+					if (index > 0) {
+						return minStr.subSequence(0, index).toString();
+					} else {
+						return "";
+					}
+				}
+			}
+		}
+		return minStr;
+	}
 }
---
+++ leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0020.java
@@ -0,0 +1,35 @@
+package com.sly.leetcode.algorithms.simple;
+
+/**
+ * <p>给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。</p>
+ * <p>有效字符串需满足：</p>
+ * <p>左括号必须用相同类型的右括号闭合。</p>
+ * <p>左括号必须以正确的顺序闭合。</p>
+ * <p>注意空字符串可被认为是有效字符串。</p>
+ * <p>示例 1:</p>
+ * <p>输入: "()"</p>
+ * <p>输出: true</p>
+ * <p>示例 2:</p>
+ * <p>输入: "()[]{}"</p>
+ * <p>输出: true</p>
+ * <p>示例 3:</p>
+ * <p>输入: "(]"</p>
+ * <p>输出: false</p>
+ * <p>示例 4:</p>
+ * <p>输入: "([)]"</p>
+ * <p>输出: false</p>
+ * <p>示例 5:</p>
+ * <p>输入: "{[]}"</p>
+ * <p>输出: true</p>
+ * @author sly
+ * @time 2019年8月13日
+ */
+public class Problem0020 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static boolean isValid(String s) {
+        return false;
+    }
+}
