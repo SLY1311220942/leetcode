2d43cb21a1411bd924ec4f307d2b4991e9627cf7
盛领宜 <sheng.ly_wh@www.sand.com>
2019-10-25 10:21:15 +0800

    提交

--- leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0053.java
+++ leetcode/src/main/java/com/sly/leetcode/algorithms/simple/Problem0053.java
@@ -1,26 +1,46 @@
 package com.sly.leetcode.algorithms.simple;
 
 /**
- * <p>53.最大子序和</p>
- * <p>给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。</p>
- * <p>示例:</p>
- * <p>输入: [-2,1,-3,4,-1,2,1,-5,4],</p>
- * <p>输出: 6</p>
- * <p>解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。</p>
- * <p>进阶:</p>
- * <p>如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。</p>
+ * <p>
+ * 53.最大子序和
+ * </p>
+ * <p>
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * </p>
+ * <p>
+ * 示例:
+ * </p>
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ * </p>
+ * <p>
+ * 输出: 6
+ * </p>
+ * <p>
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * </p>
+ * <p>
+ * 进阶:
+ * </p>
+ * <p>
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * </p>
+ * 
  * @author sly
  * @time 2019年9月4日
  */
 public class Problem0053 {
-	
+
 	public static void main(String[] args) {
-		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 		System.out.println(maxSubArray(nums));
 	}
-	
+
 	/**
-	 * <p>暴力方式不可取，超出时间限制.....</p>
+	 * <p>
+	 * 暴力方式不可取，超出时间限制.....
+	 * </p>
+	 * 
 	 * @param nums
 	 * @return
 	 */
@@ -29,25 +49,43 @@ public class Problem0053 {
 		for (int num : nums) {
 			max += num;
 		}
-		
+
 		for (int i = 0; i < nums.length; i++) {
-			
+
 			for (int j = 0; j < nums.length - i; j++) {
 				int total = nums[j];
 				for (int k = 1; k <= i; k++) {
 					total += nums[j + k];
 				}
-				if(total > max) {
+				if (total > max) {
 					max = total;
 				}
-				
+
 			}
-			
+
 		}
-        return max;
-    }
-	
+		return max;
+	}
+
+	/**
+	 * 推荐解法(我还是没能理解)
+	 * 
+	 * @param nums
+	 * @return
+	 * @author sly
+	 * @time 2019年10月25日
+	 */
 	public static int maxSubArray2(int[] nums) {
-		return 0;
+		int res = nums[0];
+		int sum = 0;
+		for (int num : nums) {
+			if (sum > 0) {
+				sum += num;
+			} else {
+				sum = num;
+			}
+			res = Math.max(res, sum);
+		}
+		return res;
 	}
 }
